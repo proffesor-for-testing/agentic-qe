c6273ec85de9e86f45c6f03fb9edc8a545ba05f7
Profa <spiridonovdragan@gmail.com>
2025-11-13 08:06:08 +0000

    cleanup

--- new-test/petstore-app/src/core/sync/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * QUIC Synchronization Module
- * Exports QUIC server and connection classes for pattern synchronization
- */
-
-export { QUICServer } from './QUICServer';
-export { QUICConnection } from './QUICConnection';
-
-export * from '../../types/quic';
